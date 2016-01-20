13705f78a8f322b5cdc865f9fc780bed35fe59b3
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2016-01-20 14:34:18 -0800

    Insert another test row.

--- create.inc.php
+++ create.inc.php
@@ -13,9 +13,15 @@ try {
     die();
 }
 
+// Drop tables
+
+$db->exec('DROP TABLE IF EXISTS group_members');
+$db->exec('DROP TABLE IF EXISTS groups');
+$db->exec('DROP TABLE IF EXISTS courses');
+$db->exec('DROP TABLE IF EXISTS users');
+
 // Create tables
 
-$db->exec('DROP TABLE IF EXISTS users');
 $db->exec('
 CREATE TABLE users (
     id INTEGER,
@@ -29,24 +35,34 @@ CREATE TABLE users (
     -- FOREIGN KEY (standing_id) REFERENCES class_standings (id),
 ) ENGINE=InnoDB, CHARACTER SET=UTF8');
 
-$db->exec('DROP TABLE IF EXISTS classes');
 $db->exec('
-CREATE TABLE classes (
+CREATE TABLE courses (
     id INTEGER,
     department VARCHAR(255), -- eg MTH
     course VARCHAR(255), -- eg MTH252
     year INTEGER, -- eg 2016
     title VARCHAR(255), -- eg Integral Calculus
 
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB, CHARACTER SET=UTF8');
 
-$db->exec('DROP TABLE IF EXISTS groups');
 $db->exec('
 CREATE TABLE groups (
     id INTEGER,
+    course_id INTEGER,
 
-    PRIMARY KEY(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (course_id) REFERENCES courses (id)
+) ENGINE=InnoDB, CHARACTER SET=UTF8');
+
+$db->exec('
+CREATE TABLE group_members (
+    group_id INTEGER,
+    user_id INTEGER,
+
+    PRIMARY KEY (group_id, user_id),
+    FOREIGN KEY (group_id) REFERENCES groups (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 ) ENGINE=InnoDB, CHARACTER SET=UTF8');
 
 // Insert values
@@ -63,20 +79,47 @@ $stmt->bindValue("email", "ekstedta@oregonstate.edu");
 $stmt->bindValue("name", "Andrew Ekstedt");
 $stmt->execute();
 
+$stmt = $db->prepare("INSERT INTO courses (id, department, course, year, title) VALUES (:id, :department, :course, :year, :title)");
+
+$stmt->bindValue("id", 1);
+$stmt->bindValue("department", "CS");
+$stmt->bindValue("course", "CS290");
+$stmt->bindValue("year", 2016);
+$stmt->bindValue("title", "Web Development");
+$stmt->execute();
+
 // Display values
+
+echo "<!doctype html>\n";
 
 $stmt = $db->prepare("SELECT * FROM users");
 $stmt->execute();
-echo "<!doctype html>\n";
-echo '<table border=1>'."\n";
-echo '  <tr><th>id<th>name<th>email</tr>'."\n";
+echo "<h1>Users</h1>\n";
+echo "<table border=1>\n";
+echo "  <tr><th>id<th>name<th>email</tr>\n";
 foreach ($stmt as $row) {
     echo '  <tr>';
     echo '<td>'.htmlspecialchars($row['id']).'</td>';
     echo '<td>'.htmlspecialchars($row['name']).'</td>';
     echo '<td>'.htmlspecialchars($row['email']).'</td>';
-    echo '</tr>'."\n";
+    echo "</tr>\n";
 }
-echo '</table>';
+echo "</table>\n";
+
+$stmt = $db->prepare("SELECT * FROM courses");
+$stmt->execute();
+echo "<h1>Courses</h1>\n";
+echo "<table border=1>\n";
+echo "  <tr><th>id<th>dept<th>course<th>title<th>year</tr>\n";
+foreach ($stmt as $row) {
+    echo '  <tr>';
+    echo '<td>'.htmlspecialchars($row['id']).'</td>';
+    echo '<td>'.htmlspecialchars($row['department']).'</td>';
+    echo '<td>'.htmlspecialchars($row['course']).'</td>';
+    echo '<td>'.htmlspecialchars($row['title']).'</td>';
+    echo '<td>'.htmlspecialchars($row['year']).'</td>';
+    echo "</tr>\n";
+}
+echo "</table>\n";
 
 ?>
