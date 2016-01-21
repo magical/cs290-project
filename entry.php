a395dda91bb1675c42f1d5ab7edf881e18f098e8
elliomic <elliomic@onid.orst.edu>
2016-01-20 22:18:21 -0800

    Added navigation links and changed file names

---
+++ entry.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<title>CS 290</title>
+	<?php include('_head.html');?>
+  </head>
+
+  <body>
+	
+	<?php include('_nav.html');?>
+
+	<?php include('_footer.php');?>
+
+  </body>
+</html>
