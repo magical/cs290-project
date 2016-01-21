089662dc54884a24a888a0a6b07dfd1160084506
elliomic <elliomic@onid.orst.edu>
2016-01-20 21:56:33 -0800

    Moved navbar, footer, and includes to header and footer include files

--- index.php
+++ index.php
@@ -1,42 +1,13 @@
+<!DOCTYPE html>
 <html>
   <head>
-	<title>CS 290</title>
-	<link rel="icon"
-		  type="image/png"
-		  href="favicon.png">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<?php include('_head.html');?>
 	<link rel="stylesheet" href="css/jumbotron.css">
-	<link rel="stylesheet" href="css/justified-nav.css">
   </head>
 
   <body>
-	<nav class="navbar navbar-inverse navbar-fixed-top">
-	  <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#">Study Group Finder</a>
-		</div>
-		<div id="navbar" class="navbar-collapse collapse">
-		  <div class="navbar-form navbar-right">
-			<a class="btn btn-success" href="signin.html" role="button">Sign in</a>
-		  </div>
-		</div>
-	  </div>
-	</nav>
 
-	<div class="container">
-	  
-      <div class="masthead">
-        <nav>
-          <ul class="nav nav-justified">
-            <li><a href="#">Home</a></li>
-            <li><a href="create.php">Create</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-			<li><a href="#">Link</a></li>
-          </ul>
-        </nav>
-      </div>
+	<?php include('_header.html');?>
 
       <div class="jumbotron">
         <h1>Welcome!</h1>
@@ -69,15 +40,7 @@
 		</div>
 	  </div>
 
-	  <footer class="footer">
-		<div class="container">
-		  <p class="text-muted"><?php
-            echo date('r');
-          ?></p>
-		</div>
-	  </footer>
-
-	</div>
+	  <?php include('_footer.php');?>
 
   </body>
 </html>
