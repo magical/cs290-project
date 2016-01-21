db961fb8124677e8f644088c09e25bb369cf0e85
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2016-01-21 13:48:59 -0800

    includes/*: Convert tabs to spaces.
    Forgot to add these in my last commit.

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,9 +1,9 @@
-	  <footer class="footer">
-		<div class="container">
-		  <p class="text-muted"><?php
+      <footer class="footer">
+        <div class="container">
+          <p class="text-muted"><?php
             echo date('r');
           ?></p>
-		</div>
-	  </footer>
+        </div>
+      </footer>
 
-	</div>
+    </div>
