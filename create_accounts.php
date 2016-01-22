61af0491a491785fc7dc98b6dde81f92fad05b42
elliomic <elliomic@onid.orst.edu>
2016-01-21 23:46:56 -0800

    Now uses password hashes

--- create_accounts.php
+++ create_accounts.php
@@ -22,9 +22,18 @@ $db->exec('
 CREATE TABLE accounts (
     email VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
-    pass_hash VARCHAR(255) NOT NULL,
+    pass_hash VARCHAR(60) NOT NULL,
 
     PRIMARY KEY (email)
 ) ENGINE=InnoDB, CHARACTER SET=UTF8');
+
+// Insert values
+
+$stmt = $db->prepare("INSERT INTO accounts (email, username, pass_hash) VALUES (:email, :username, :pass_hash)");
+
+$stmt->bindValue("email", "user@host.tld");
+$stmt->bindValue("username", "user");
+$stmt->bindValue("pass_hash", password_hash("pass", PASSWORD_BCRYPT));
+$stmt->execute();
 
 ?>
--- verify.php
+++ verify.php
@@ -1,10 +1,30 @@
 <?php
 session_start();
-if ($_POST["email"] == "user@host.tld" && $_POST["password"] == "pass") {
-    $_SESSION["isLoggedIn"] = 1;
-} else {
-	$_SESSION["isLoggedIn"] = 0;
+
+require_once "config.php";
+
+// Connect to the database
+
+try {
+	$db = new PDO($dsn, $dbuser, $dbpass);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	echo "Error connecting to database";
+	die();
 }
+
+//TODO: Protect against SQL injection
+$stmt = $db->prepare("SELECT * FROM accounts WHERE email='".$_POST["email"]."'");
+$stmt->execute();
+
+foreach ($stmt as $row) {
+	if (password_verify($_POST["password"], $row["pass_hash"])) {
+		$_SESSION["isLoggedIn"] = 1;
+	} else {
+		$_SESSION["isLoggedIn"] = 0;
+	}
+}
+
 header("Location: index.php");
 exit();
 ?>
