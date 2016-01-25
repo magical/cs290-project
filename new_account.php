7cdbcc815b93ea3ac79d3d2a0e3c6a544514722d
elliomic <elliomic@onid.orst.edu>
2016-01-24 23:51:23 -0800

    Validates form server side before adding to database

--- new_account.php
+++ new_account.php
@@ -1,18 +1,20 @@
 <?php
 
-require_once "includes/all.php";
-
-// Connect to the database
-
-$db = connect_db();
-
-// Insert values
-
-$stmt = $db->prepare("INSERT INTO users (email, password_hash) VALUES (:email, :password_hash)");
-
-$stmt->bindValue("email", $_POST["email"]);
-$stmt->bindValue("password_hash", password_hash($_POST["password"], PASSWORD_BCRYPT));
-$stmt->execute();
+if ($_POST["password"] == $_POST["passwordConfirm"]) {
+	require_once "includes/all.php";
+	
+	// Connect to the database
+	
+	$db = connect_db();
+	
+	// Insert values
+	
+	$stmt = $db->prepare("INSERT INTO users (email, password_hash) VALUES (:email, :password_hash)");
+	
+	$stmt->bindValue("email", $_POST["email"]);
+	$stmt->bindValue("password_hash", password_hash($_POST["password"], PASSWORD_BCRYPT));
+	$stmt->execute();
+}
 
 header("Location: signin.php");
 exit();
