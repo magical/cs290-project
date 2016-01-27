d0ad5820af548a4f5a7638459798afda8a3c571d
elliomic <elliomic@onid.orst.edu>
2016-01-26 17:17:06 -0800

    Now checks for duplicate emails

--- new_account.php
+++ new_account.php
@@ -2,18 +2,27 @@
 
 if ($_POST["password"] == $_POST["passwordConfirm"] && $_SERVER["HTTP_REFERER"] == "http://web.engr.oregonstate.edu/~elliomic/cs290-project/signup.php") {
 	require_once "includes/all.php";
-	
+
 	// Connect to the database
-	
+
 	$db = connect_db();
-	
-	// Insert values
-	
-	$stmt = $db->prepare("INSERT INTO users (email, password_hash) VALUES (:email, :password_hash)");
-	
+
+	// Retrieve id
+
+	$stmt = $db->prepare("SELECT id FROM users WHERE email=:email");
+
 	$stmt->bindValue("email", $_POST["email"]);
-	$stmt->bindValue("password_hash", password_hash($_POST["password"], PASSWORD_BCRYPT));
-	$stmt->execute();
+
+	if (!$stmt->fetch()) {
+
+		// Insert values
+
+		$stmt = $db->prepare("INSERT INTO users (email, password_hash) VALUES (:email, :password_hash)");
+
+		$stmt->bindValue("email", $_POST["email"]);
+		$stmt->bindValue("password_hash", password_hash($_POST["password"], PASSWORD_BCRYPT));
+		$stmt->execute();
+	}
 }
 
 header("Location: signin.php");
