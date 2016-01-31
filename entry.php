5515e9a3eef72a3eeb0b913d3a0dd6d78bbcb9cd
Jacob Mahugh <mahughj@oregonstate.edu>
2016-01-30 23:43:06 -0800

    Starting to add data to tables - need different columns on table (Additional, I would say)

--- entry.php
+++ entry.php
@@ -1,5 +1,5 @@
 <?php session_start();?>
-<?php require_once 'includes/functions.php'; ?>
+<?php require_once 'includes/all.php'; ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -34,6 +34,12 @@
 		<option value="Sci"> Science </option>
 		<option value="Vet"> Veterinary Medicine </option>
 	</select>
+	<select name="campus">
+		<option value="0"> Select Campus </option>
+		<option value="Cor"> Corvallis (Main)</option>
+		<option value="Cas"> Cascades </option>
+		<option value="Onl"> Online </option>
+	</select>
 	<h4>Enter your two best times in the format DAY, TI:ME XM (i.e: R, 05:00 PM) </h4>
 	<h5>Use R for Thursday, N for Sunday</h5>
 	<input type="text" name="t1"> 
@@ -50,7 +56,43 @@
 			if (array_key_exists("t2", $_REQUEST))
 				echo ' '.htmlspecialchars($_REQUEST["t2"]);
 
+			if ($_REQUEST["standingselect"] == "0") {
+				echo "<p text-color="Red"> ERROR: Please choose class standing </p>";
+			}
+			elseif ($_REQUEST["collegeselect"] == "0") {
+				echo "<p text-color="red"> ERROR: Please choose college </p>";
+			}
+			elseif($_REQUEST["campus"] == "0") {
+				echo "<p text-color="red"> ERROR: Please choose campus </p>";
+			}
+			else {
+			$db = connect_db();
+			//RETRIEVE USERID FROM USERS SESSION - CODE HERE
+			
+			//UP THERE SEE THAT NOTE PLEASE :)
+			//This is a temporary user ID
+			
+			
+			$userID = 2;
+			
+			
+/*				$dbhost = 'odu';
+			$dbname = 'mb';
+			$dbuser = 'mb';
+			$dbpass = 'HA NICE TRY';
 
+			$mysql_handle = mysql_connect($dbhost, $dbuser, $dbpass)
+	   	 or die("Error connecting to database server");
+
+			mysql_select_db($dbname, $mysql_handle)
+		    or die("Error selecting database: $dbname");
+
+			echo 'Successfully connected to database!';
+			
+			mysql_close($mysql_handle);
+			
+*/
+			}
 			//insert this into table 
 
 		?>
