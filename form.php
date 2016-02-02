7be45daf1b86f2dd9ef8ab3340f881d3ecd294a9
Oreko <or3ko@yahoo.co.jp>
2016-02-01 21:58:12 -0800

    Search form added

---
+++ form.php
@@ -0,0 +1,19 @@
+<body>
+	<title> Search Groups </title>
+
+	<h2> Basic Search Page </h2>
+	
+	<form action = "search.php" method = "get">
+		Group Name: <br>
+		<input type = "text" name = "group" placeholder = "Group Name"><br><br>
+		<input type = "submit" value = "Search"><br>
+	</form>
+	
+	<form action = "search.php" method = "get">
+	<h3>
+		Or alternatively show all groups: </br>		
+	</h3>
+	<input type = "submit" name='all'> </br>
+	</form>
+	
+</body>
