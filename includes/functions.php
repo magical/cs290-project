303693ee3ff067c693121af1586c501b0b7c1a41
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2016-02-01 17:16:33 -0800

    profile.php: Basic user profile page.

--- includes/functions.php
+++ includes/functions.php
@@ -21,3 +21,23 @@ function connect_db() {
   }
   return $db;
 }
+
+function get_user($db, $id) {
+  $stmt = $db->prepare('SELECT * FROM users WHERE id = :id');
+  $stmt->bindValue("id", $id);
+  $stmt->execute();
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+  return $row;
+}
+
+function get_user_courses($db, $user_id) {
+  $stmt = $db->prepare('
+    SELECT courses.*
+    FROM courses
+    JOIN user_courses ON user_courses.course_id = courses.id
+    WHERE user_id = :user_id');
+  $stmt->bindValue("user_id", $user_id);
+  $stmt->execute();
+  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  return $rows;
+}
