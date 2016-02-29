ecc9d7f2ba028f5370c2dcb77827b6d674ed852e
jamesun1989 <jamesun1989@gmail.com>
2016-02-29 12:38:10 -0800

    group_entry.php

--- group_entry.php
+++ group_entry.php
@@ -1,6 +1,14 @@
 <?php require_once 'includes/all.php';
 
+//session_start();
+
 	$db=connect_db();
+
+
+	$gid=$_SESSION['selgid'];
+
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
 
 	$gname=$_POST['groupnm'];
 	$gmsg=$_POST['gmsg'];
@@ -10,65 +18,45 @@
 	$newday=$_POST['week'];
 	$newtime=$_POST['selt'];
 
-
-	$uid=get_logged_in_user_id();
-
-	$gid="SELECT group_id FROM group_members WHERE user_id=$uid";
-
 	$campus="SELECT name FROM campuses WHERE id=$newcam";
 
 
 	if(!empty($gname)){
-		foreach($db->query($gid) as $groupid){
-			$groid=$groupid['group_id'];
-			$stmt=$db->prepare("UPDATE groups SET name='$gname' WHERE id=$groid");
+			$stmt=$db->prepare("UPDATE groups SET name='$gname' WHERE id=$gid");
 			$stmt->execute();
 
-			header("Location:group.php");			
-		}
+			header("Location:group.php?id=$gid");			
 	}
 
 	if(!empty($gmsg)){
-		foreach($db->query($gid) as $groupid){
-			$groid=$groupid['group_id'];
-			$stmt=$db->prepare("UPDATE groups SET blurb='$gmsg' WHERE id=$groid");
+			$stmt=$db->prepare("UPDATE groups SET blurb='$gmsg' WHERE id=$gid");
 			$stmt->execute();
 
-			header("Location:group.php");			
-		}
+			header("Location:group.php?id=$gid");			
 	}
 
 	if(!empty($newsou)){
-		foreach($db->query($gid) as $groupid){
-			$groid=$groupid['group_id'];
-			$stmt=$db->prepare("UPDATE groups SET course_id='$newsou' WHERE id=$groid");
+			$stmt=$db->prepare("UPDATE groups SET course_id='$newsou' WHERE id=$gid");
 			$stmt->execute();
 
-			header("Location:group.php");			
-		}
+			header("Location:group.php?id=$gid");
 	}
 
 	if(!empty($newcam) && !empty($newbdg)){
-		foreach($db->query($gid) as $groupid){
-			$groid=$groupid['group_id'];
 			foreach($db->query($campus) as $cam){
 				$camnm=$cam['name'];
-				$stmt=$db->prepare("UPDATE groups SET place='$camnm $newbdg' WHERE id=$groid");
+				$stmt=$db->prepare("UPDATE groups SET place='$camnm $newbdg' WHERE id=$gid");
 				$stmt->execute();
-				header("Location:group.php");			
-			}			
+				header("Location:group.php?id=$gid");	
 		}
 
 	}
 
 	if(!empty($newday) && !empty($newtime)){
-		foreach($db->query($gid) as $groupid){
-			$groid=$groupid['group_id'];
-			$stmt=$db->prepare("UPDATE groups SET time='$newday $newtime:00' WHERE id=$groid");
+			$stmt=$db->prepare("UPDATE groups SET time='$newday $newtime:00' WHERE id=$gid");
 			$stmt->execute();
 
-			header("Location:group.php");
-		}
-
+			header("Location:group.php?id=$gid");
 	}
+}
 ?>
