69e5e91f2f15598ec8bbb54b3a4cce87557ed9a4
Jacob Mahugh <mahughj@oregonstate.edu>
2016-03-05 22:54:22 -0800

    I got bored and felt slightly inspired! :)

--- about.php
+++ about.php
@@ -8,7 +8,27 @@
 
   <body>
     <?php include 'includes/_nav.php';?>
-
+		<div class='jumbotron'>
+		<h1 align='center'> The Fellowship of the Wingding </h1>
+		<h3 align='center'> A Tale of Everyday Heroes </h3>
+		<p> 
+		Our tale begins one fateful day in a classroom in the LInC, a vast building with severable unexplored regions. One day a giant monster named Scaffidi's Project appeared and started threatening the class, brandishing a flaming sword. 
+		In order to slay this monster, a group of heroes banded together to create a tool that would allow other groups of heroes to band together. A group of many talents, one determined they should be the <em> Fellowship of the Wingding</em>.
+		The Fellowship, after several sleepness nights, somber meetings and trainging sessions confronted the monster, slaying it in an epic, prolonged 10 week battle. All that is left of the monster are the scars the the Fellowship will wear forever,
+		and the tool that the Fellowship created to slay this foul creature. To this day, their legacy carries on in every group of heroes that bands together to defeat the monsters we now call classes. 
+		</p>
+		<p>
+		For the annals of history, here are the members of the Fellowship of the Wingding:
+		</p>
+		<ul>
+			<li>Brandon Chatham, a swordsman equally quick with his sword and feet; on the eve of the final battle he stared down the monster and did not blink</li>
+			<li>Andrew Ekstedt, a sorceror of untold power; his knowledge of the arcane complimented his natural ability as he led the Fellowship </li>
+			<li>Michael Elliott, an assassin that struck from the shadows; legend holds that it was his blow that felled the monster</li>
+			<li>Jacob Mahugh, an elaborate storyteller that wielded a bo staff of fire; the only thing more fiery was his wit</li>
+			<li>Ian McQuoid, a druid with powerful ability to control plants; legend holds that he left his order, which was focused on plants, to aid the Fellowship</li>
+			<li>Xiaoli Sun, an archer of uncanny accuracy; his covering fire allowed the rest of the Fellowship to close in the for final blow</li>
+		</ul>
+		</div>
     <?php include 'includes/_footer.php';?>
   </body>
 </html>
