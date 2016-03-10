29d3184e8756be87ab45904bf801fd8a45308c2b
Jacob Mahugh <mahughj@oregonstate.edu>
2016-03-09 21:37:24 -0800

    Added about and source to footer

--- includes/_footer.php
+++ includes/_footer.php
@@ -2,7 +2,10 @@
         <div class="container">
           <p class="text-muted"><?php
             echo date('r');
-          ?></p>
+          ?>
+			 	<a href='about.php'> More about us </a>
+				<a href='github.com/magical/cs290-project'>Check out our source code</a>
+			 </p>
         </div>
       </footer>
 
